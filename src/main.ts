0af2069dfa0f6d8fc1d05670962722918207d397
lam20042001 <lam.lylythanhlam@hcmut.edu.vn>
2024-12-23 10:02:20 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:4000', // Allow requests from this origin
-    methods: 'GET,POST', // Allow these HTTP methods
+    methods: 'GET,POST,PUT,DELETE', // Allow these HTTP methods
     allowedHeaders: 'Content-Type,Authorization', // Allow these headers
   });
   app.useGlobalFilters(new AllExceptionsFilter());
